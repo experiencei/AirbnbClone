8a63d7a86334824c38866c552a42dd8b6db2e0b5
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 10:06:35 +0100

    creating reusable Largecard components

--- components/map/Map.js
+++ components/map/Map.js
@@ -1,2 +1,22 @@
 import { useState , useEffect} from "react";
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import getCenter from "geolib/es/getCenter";
+
+function Map() {
+    return (
+        <ReactMapGL
+            width="100%"
+            height="100%"
+            mapStyle="mapbox://styles/ryandungl3/cks5h97v61bnt18p5kpzhxqvu"
+            mapboxApiAccessToken={process.env.mapbox_key}
+            onViewportChange={(nextViewport) => setViewport(nextViewport)}
+            {...viewport}
+    > 
+    
+    
+    
+    </ReactMapGL>
+    )
+}
+
+export default Map
