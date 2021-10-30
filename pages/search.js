ab57c0759798f782742c0cce1bc343adbdae2488
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 11:02:18 +0100

    working on the homepage Index.js

--- pages/search.js
+++ pages/search.js
@@ -24,13 +24,13 @@ function search({ searchResults }) {
             Stay in {location}
           </h1>
           <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
-          <p className="button">Cancellation Flexibility</p>
+           <p className="button">Cancellation Flexibility</p>
             <p className="button">Type of Place</p>
             <p className="button">Price</p>
             <p className="button">Rooms and Beds</p>
             <p className="button">More filters</p>
-
           </div>
+          
             </section>
 
          </main>
