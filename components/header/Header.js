dc9c37248f5d26945bccb0d4a2aa179073dde574
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 09:22:27 +0100

    working on the footer components

--- components/header/Header.js
+++ components/header/Header.js
@@ -6,3 +6,7 @@ import {
     UserCircleIcon,
     UsersIcon,
   } from "@heroicons/react/solid";
+  import { useState } from "react";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
