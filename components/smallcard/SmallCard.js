5fde0e002685b7d6cc4bb218aefeb10c841cae44
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 10:23:56 +0100

    working on the medium cards for rendering

--- components/smallcard/SmallCard.js
+++ components/smallcard/SmallCard.js
@@ -4,8 +4,9 @@ function SmallCard({ img, location, distance }) {
     return (
         <div  className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition duration-200 ease-out">
              <div className="relative h-16 w-16">
-                 
+               <Image src={img} layout="fill" className="rounded-lg" />
              </div> 
+             
         </div>
     )
 }
