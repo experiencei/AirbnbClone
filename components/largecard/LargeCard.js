8e0a906f438f40e4d3b2649d0e6f84d3dd859ac3
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 09:54:35 +0100

    creating reusable info card components

--- components/largecard/LargeCard.js
+++ components/largecard/LargeCard.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 function LargeCard({ img, title, description, buttonText }) {
     return (
-        <div>
-            
-        </div>
+        <section  className="relative py-16 cursor-pointer">
+            <div className="relative h-96 min-w-[300px ]">
+
+            </div>
+        </section>
     )
 }
 
