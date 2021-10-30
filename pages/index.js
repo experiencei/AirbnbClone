656d7bbc7f08287fb7fddc45101ffbecdfd04c7e
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 10:40:46 +0100

    small component resuable card

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,10 @@ export default function Home() {
           </div>
         </section>
         <section>
+        <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
           
+          </div>
         </section>
         </main>
     </div>
