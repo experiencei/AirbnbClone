ccf5b3b26bfe5fbdcfd64920733601ceb1d0870c
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 09:25:09 +0100

    working on the footer components

--- components/header/Header.js
+++ components/header/Header.js
@@ -11,3 +11,17 @@ import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from "react-date-range";
 import { useRouter } from "next/dist/client/router";
+
+function Header() {
+    return (
+        <header  className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+
+            {/* left */}
+             <div className="relative flex items-center h-10 cursor-pointer my-auto">
+                 
+             </div> 
+        </header>
+    )
+}
+
+export default Header
