276a8b14de51f194f9955be15e29a6c99cdd0eff
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 09:25:40 +0100

    working on the footer components

--- components/header/Header.js
+++ components/header/Header.js
@@ -18,7 +18,13 @@ function Header() {
 
             {/* left */}
              <div className="relative flex items-center h-10 cursor-pointer my-auto">
-                 
+             <Image
+                src="https://links.papareact.com/qd3"
+                layout="fill"
+                objectFit="contain"
+                objectPosition="left"
+                onClick={() => router.push("/")}
+                />
              </div> 
         </header>
     )
