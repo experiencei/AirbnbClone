9d17a87062f748ed3b4b0f4f9f482d579c59999f
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 09:42:10 +0100

    implementing date range to the header component

--- components/infocard/InfoCard.js
+++ components/infocard/InfoCard.js
@@ -1,3 +1,13 @@
 import Image from "next/image";
 import { HeartIcon } from "@heroicons/react/outline";
 import { StarIcon } from "@heroicons/react/solid";
+
+function InfoCard({}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default InfoCard
