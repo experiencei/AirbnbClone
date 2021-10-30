5e62a717f5880cf3b5800e5a681fc351db0375cd
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 08:51:14 +0100

    tailwind scrollbar hide implementation

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiZXhwZXJpZW5jZWlxIiwiYSI6ImNrdmRnampkbTM1czMyb2x1Z2x0azBzY3gifQ.S25cdzqM2_4_U4YBxDJk-w",
+  },
 }
