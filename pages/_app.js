148573772fb6aa91116c140866082ec332195d70
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 08:57:04 +0100

    tailwind scrollbar hide implementation

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,14 @@ import '../styles/globals.css'
 import ProgressBar from "@badrap/bar-of-progress";
 import Router from "next/router";
 
+const progress = new ProgressBar({
+  size: 4,
+  color: "#FE595E",
+  className: "z-50",
+  delay: 100,
+});
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
