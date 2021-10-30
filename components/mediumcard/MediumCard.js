7560c57fac7ef8c29ab6093b7f2756c520110a6d
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 10:15:55 +0100

    implementing map-geolocation & geolib for distance calc

--- components/mediumcard/MediumCard.js
+++ components/mediumcard/MediumCard.js
@@ -4,7 +4,7 @@ function MediumCard({ img, title }) {
     return (
         <div  className="cursor-pointer hover:scale-105 transition duration-300 ease-out">
             <div className="relative h-80 w-80">
-
+              <Image src={img} layout="fill" className="rounded-xl" />
             </div>
         </div>
     )
