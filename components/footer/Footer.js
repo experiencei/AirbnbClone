67ff6e9210e7979f83298d519ec7c06c2fd04892
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 09:14:52 +0100

    working on the banner components

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -11,6 +11,11 @@ function Footer() {
             </div>
             <div className="space-y-4 text-sm text-gray-800">
             <h5 className="font-bold">ABOUT</h5>
+            <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
             </div>
         </div>
     )
