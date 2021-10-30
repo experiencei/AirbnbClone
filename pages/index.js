cf8eb4d926b197d13295aa81a86228a3635067ce
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 10:39:23 +0100

    small component resuable card

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
       <Banner />
         <main  className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
-          
+        <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
         </section>
         </main>
     </div>
