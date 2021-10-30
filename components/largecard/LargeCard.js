76b55f6c9a92706a436f2f19b850918540e6439e
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 09:57:24 +0100

    creating reusable Largecard components

--- components/largecard/LargeCard.js
+++ components/largecard/LargeCard.js
@@ -13,6 +13,9 @@ function LargeCard({ img, title, description, buttonText }) {
             <div   className="absolute top-1/3 left-12">
               <h3 className="text-4xl mb-3 w-64 font-semibold">{title}</h3>
               <p>{description}</p>
+              <button className="text-sm text-white bg-gray-900 rounded-lg mt-5 px-4 py-2">
+                {buttonText}
+            </button>
             </div>
         </section>
     )
