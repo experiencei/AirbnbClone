bf52b9fb050b6a9d4e30c8578580f344bc0bc320
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 09:12:59 +0100

    working on the banner components

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,7 +1,9 @@
 function Footer() {
     return (
-        <div>
-            
+        <div  className="grid grid-cols-1 md:grid-cols-4 gap-10 py-14 px-24 bg-gray-100 place-items-center">
+            <div className="space-y-4 text-sm text-gray-800">
+                
+            </div>
         </div>
     )
 }
