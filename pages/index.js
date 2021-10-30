50440115d3e860b66be5b598154ae07f44b4b4aa
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 10:39:44 +0100

    small component resuable card

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,11 @@ export default function Home() {
         <main  className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
         <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map((item) => {
+              return <SmallCard key={item.img} {...item} />;
+            })}
+          </div>
         </section>
         </main>
     </div>
