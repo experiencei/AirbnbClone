1499bc33f2f196bad2b29b52d8b47937f6162b6f
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 09:09:21 +0100

    working on the banner components

--- components/banner/Banner.js
+++ components/banner/Banner.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function Banner() {
     return (
-        <div>
+        <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
             
         </div>
     )
