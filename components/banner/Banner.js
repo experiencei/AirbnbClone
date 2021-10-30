0cef697a2d690763dafc5836e9fba3dc1f17e2cb
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 09:09:48 +0100

    working on the banner components

--- components/banner/Banner.js
+++ components/banner/Banner.js
@@ -3,7 +3,13 @@ import Image from "next/image";
 function Banner() {
     return (
         <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
-            
+            <Image
+            src="https://links.papareact.com/0fm"
+            layout="fill"
+            objectFit="cover"
+              />
+
+              
         </div>
     )
 }
