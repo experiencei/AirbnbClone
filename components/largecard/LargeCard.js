7ef68474aa54eb2317fd14c700cc6bdfb79fb867
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 09:56:05 +0100

    creating reusable info card components

--- components/largecard/LargeCard.js
+++ components/largecard/LargeCard.js
@@ -11,7 +11,7 @@ function LargeCard({ img, title, description, buttonText }) {
           />
             </div>
             <div   className="absolute top-1/3 left-12">
-
+              <h3 className="text-4xl mb-3 w-64 font-semibold">{title}</h3>
             </div>
         </section>
     )
