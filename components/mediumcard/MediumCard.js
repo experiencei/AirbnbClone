475260424acd51ce7cf1261e533d9347813bad3e
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 10:19:51 +0100

    working on the medium cards for rendering

--- components/mediumcard/MediumCard.js
+++ components/mediumcard/MediumCard.js
@@ -6,6 +6,7 @@ function MediumCard({ img, title }) {
             <div className="relative h-80 w-80">
               <Image src={img} layout="fill" className="rounded-xl" />
             </div>
+            <h3 className="text-2xl mt-3">{title}</h3>
         </div>
     )
 }
