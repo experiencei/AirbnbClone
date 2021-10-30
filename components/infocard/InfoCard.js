a377c0aedb5024f782d5591e9c693a455358e13c
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 09:48:53 +0100

    implementing date range to the header component

--- components/infocard/InfoCard.js
+++ components/infocard/InfoCard.js
@@ -34,7 +34,11 @@ function InfoCard({
                 <div className="border-b w-10 pt-2 border-red-400"></div>
                 <p className="pt-2 text-sm text-gray-500 flex-grow">{description}</p>
                 <div className="flex justify-between pt-5">
-                    
+                <p className="flex items-center self-end">
+                    <StarIcon className="h-5 text-red-400" />
+                    {star}
+                </p>  
+                
                 </div>
              </div>
         </div>
