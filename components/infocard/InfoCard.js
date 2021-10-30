83ebd7afedf4d539f1f71e697e696439d6e04dba
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 09:44:29 +0100

    implementing date range to the header component

--- components/infocard/InfoCard.js
+++ components/infocard/InfoCard.js
@@ -17,6 +17,14 @@ function InfoCard({
         <div   className="flex py-7 px-2 border-b cursor-pointer hover:opacity-80 hover:shadow-lg
           transition duration-200 ease-out first:border-t
         ">
+        <div  className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
+            <Image
+            src={img}
+            layout="fill"
+            objectFit="cover"
+            className="rounded-2xl"
+            />
+        </div>
             
         </div>
     )
