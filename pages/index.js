9b1454222f530fa86cc533d9bc94b13a439236d3
experiencei <ayelojahighbee01@gmail.com>
2021-10-30 10:37:15 +0100

    small component resuable card

--- pages/index.js
+++ pages/index.js
@@ -20,3 +20,21 @@ export default function Home() {
     </div>
   )
 }
+
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
+  );
+
+  const cardsData = await fetch("https://links.papareact.com/zp1").then((res) =>
+    res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+      cardsData,
+    },
+  };
+}
